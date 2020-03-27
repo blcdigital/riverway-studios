2f7343a0863566b56f23ddd2992ca6cb86951d98
Jasal Vadgama <jasal@blacklabelcreative.com>
2020-03-27 16:04:28 +0000

    gatsby base build

--- site/wp-content/themes/rws/searchform.php
+++
@@ -1,9 +0,0 @@
-<form method="get" id="searchform" action="http://localhost/">
-	<fieldset>
-		<p>
-			<label class="access" for="s">Search</label>
-			<input type="text" value="search" name="s" id="s" />
-			<input type="submit" id="searchsubmit" value="Search" />
-		</p>
-	</fieldset>
-</form>
